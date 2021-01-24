55fc597c7607a569c54594bec6c319f95a4e749f
Bjorn Svensson <bsvensson@esri.com>
2021-01-23 19:59:59 -0800

    Update docs/REST2020.txt

---
+++ 1-setup.bat
@@ -0,0 +1,52 @@
+@ECHO OFF
+
+:BASH
+
+  @ECHO Is there a bash.exe in the path already?
+  set BASH=bash.exe
+  %BASH% --version >nul 2>nul
+
+  if %ERRORLEVEL%==0 (goto CONTINUE)
+
+  echo Nope.  Let's look for it elsewhere...
+
+  IF EXIST "%LOCALAPPDATA%\Programs\Git\bin\bash.exe" (
+       echo Found it. Fixed.
+       SET "PATH=%LOCALAPPDATA%\Programs\Git\bin\;%PATH%"
+       goto CONTINUE
+  ) ELSE IF EXIST "%LOCALAPPDATA%\Atlassian\SourceTree\git_local\bin\bash.exe" (
+       echo Found it. Fixed.
+       SET "PATH=%LOCALAPPDATA%\Atlassian\SourceTree\git_local\bin;%PATH%"
+       goto CONTINUE
+  ) ELSE (
+      echo Sorry, can't find a bash...
+      echo Try other places:
+      where %BASH% > tempfile 2>nul
+      where /R %LOCALAPPDATA% %BASH%
+      exit 1
+  )
+  goto CONTINUE
+
+:PYTHON
+
+  @ECHO If needed, change PRO variable according to where you arcpy is...
+
+  @REM TODO - check if right python already available...
+
+  IF EXIST "%LOCALAPPDATA%\Programs\Pro\bin\Python" (
+       SET PRO=%LOCALAPPDATA%\Programs\Pro\bin\Python
+  ) ELSE IF EXIST "%ProgramFiles%\ArcGIS\Pro\bin\Python" (
+       SET PRO=%ProgramFiles%\ArcGIS\Pro\bin\Python
+  ) ELSE (
+       echo Sorry, not sure where your arcpy is...
+  )
+
+  SET "PATH=%PRO%\Scripts;%PRO%\envs\arcgispro-py3;%PATH%"
+  python --version
+
+:CONTINUE
+
+  where bash
+  bash --version | head -1
+
+@ECHO ON
---
+++ 2-run.bat
@@ -0,0 +1,12 @@
+ECHO Get the latest strava activities for ech person and save it to single line file like data\current-month-Bjorn.txt
+call 1-setup.bat
+bash StravaActivitiesByMonth.sh
+ECHO Next ... update the database from PYTHON folder on laptop
+
+if %computername%==BSVENSSON (
+  echo laptop with ESSR2020...
+  PUSHD C:\Users\bjor3345\Documents\python
+  call runall.bat
+  POPD
+  call UpdateResults.bat
+)
