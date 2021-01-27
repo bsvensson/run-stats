36aa3f9223061f73671c2bbe486a620a6f656042
Bjorn Svensson <bsvensson@esri.com>
2021-01-26 22:49:57 -0800

    finetune

--- 4-UpdateResults.bat
+++ 4-UpdateResults.bat
@@ -1,25 +1,45 @@
+@ECHO OFF
 @echo ---- This scripts copies the log from last summarize script, 
 @echo ---- and if different, updates github repo
 
   git fetch
-  git pull
+  git pull | grep -v "Already up to date"
 
   xcopy /D /V /Y C:\Users\bjor3345\Documents\PYTHON\log-summarize-REST2020_ESSR_Redlands.html docs\ESSR2020.html 
   xcopy /D /V /Y C:\Users\bjor3345\Documents\PYTHON\log-summarize-REST2020_REST_Redlands.html docs\REST2020.html 
   xcopy /D /V /Y C:\Users\bjor3345\Documents\PYTHON\log-summarize-REST2020_REST_Hulda.html docs\REST2020-Hulda.html 
-  git status | findstr /c:"modified:   docs/"
-   
-  IF %ERRORLEVEL%==0 GOTO UPDATEREPO
-  echo Already up-to-date it seems
-  goto END
 
-:UPDATEREPO
-  echo update repo...
-  git add docs/ESSR2020.html
-  git add docs/REST2020.html
-  git add docs/REST2020-hulda.html
-  git commit -m "Update stats"
-  git push
-  goto END
+  set COMMIT=0
+
+  SET FILE=ESSR2020.html
+  git status | find "%FILE%"
+  IF %ERRORLEVEL%==0 (
+    echo updating docs/%FILE%
+    git add docs/%FILE%
+    set COMMIT=1
+  )
+
+  SET FILE=REST2020.html
+  git status | find "%FILE%"
+  IF %ERRORLEVEL%==0 (
+    echo updating docs/%FILE%
+    git add docs/%FILE%
+    set COMMIT=1
+  )
+
+  SET FILE=REST2020-Hulda.html
+  git status | find "%FILE%"
+  IF %ERRORLEVEL%==0 (
+    echo updating docs/%FILE%
+    git add docs/%FILE%
+    set COMMIT=1
+  )
+
+  IF %COMMIT%==1 (
+    echo commit and push
+    git commit -m "Update stats"
+    git push
+  )
 
 :END
+@ECHO ON
